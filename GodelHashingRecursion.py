6783b089c194f8efba61ea89181697fbe77acc3f
mloumeau <matthewloumeau@gmail.com>
2021-01-09 14:53:55 -0700

    Adding recursive godel hash

---
+++ GodelHashingRecursion.py
@@ -0,0 +1,38 @@
+# #FOR LOOP MAPS
+"""
+The first mapping creates a list of all the permutations of n length,
+then joins all elements of that list into a single string.
+This allocates a new key in the dictionary, entitled with
+the name of the string being generated.
+""" 
+
+"""
+The second mapping does the same, except it accesses the values,
+not the keys. Instead of creating a string for these, we get the
+product since that is what Godel Hashes do. This end product will
+be the value to the string key generated in the first mapping.
+"""
+
+"""
+PERMUTATIONS WITH OR WITHOUT REPETITION
+"""
+from itertools import product,permutations
+from math import prod
+
+d={'Abu':2,'Til':3,'Ila':5,'Nef':7}
+newD={}
+
+def repOrNoRep(dict,x,permLength):
+    if x: return product(dict,repeat=permLength) #This produces all permutations with repetitions
+    return permutations(dict) #This produces all permutations without repetitions
+
+def recur(start,end,nD,repOrNo):
+    if start > end:
+        list(map(lambda x:print('{:16}'.format(x),'{:>6}'.format(nD[x])),dict(sorted(nD.items(),key=lambda x:x[1]))))
+    else:
+        for names in [c for c in repOrNoRep(d,repOrNo,start)]:
+            nD[(''.join(list(map(lambda i:names[i],range(start)))))]=prod(list(map(lambda i:d[names[i]],range(start))))
+        recur(start+1,end,nD,repOrNo)
+
+recur(1,4,newD,True) #args= start, end, dictionary, with or without repetitions
+
