4c1da3415ef99a6619ddcdfe1130d7f20e8fc272
mloumeau <matthewloumeau@gmail.com>
2021-01-09 15:51:52 -0700

    Adjusted original hashing document

--- GodelHashing.py
+++ GodelHashing.py
@@ -21,6 +21,9 @@ PERMUTATIONS WITH REPETITION
 """
 from itertools import product,permutations
 from math import prod
+import time
+
+start = time.time()
 
 d={'Abu':2,'Til':3,'Ila':5,'Nef':7}
 
@@ -36,34 +39,13 @@ for permLength in range(1,5):
 
 #Sorted has a keyword of 'key' as an arg. It allows to decide what you want sorted. 
 list(map(lambda x:print('{:16}'.format(x),'{:>6}'.format(newD[x])),dict(sorted(newD.items(),key=lambda x:x[1]))))
-
-
-"""
-CRAPPY WAY OF DOING THIS (WITH REPETITION)
-"""
-
-# totals={}
-# for w in d:
-#     totals[w]=d[w]
-#     for x in d:
-#         totals[x+w]=d[w]*d[x]
-#         for y in d:
-#             totals[y+x+w] = d[w]*d[x]*d[y]
-#             for z in d:
-#                 totals[z+y+x+w]=d[w]*d[x]*d[y]*d[z]
-
-# list(map(lambda x:print('{:16}'.format(x),'{:>6}'.format(newD[x])),dict(sorted(newD.items(),key=lambda x:x[1]))))
-
+stop = time.time()
+print(stop - start)
 
 """
-PERMUTATIONS WITHOUT REPETITION
-"""
+TIMES
+1-8   = 30.81
+1-9  = 121.19
+1-10 = 457.55
 
-# from itertools import product, permutations
-# from math import prod
-# d={'Abu':2,'Til':3,'Ila':5,'Nef':7}
-# newD={}
-# for permLength in range(1,5):
-#     for names in [c for c in list(permutations(d))]:
-#         newD[(''.join(list(map(lambda i:names[i],range(permLength)))))]=prod(list(map(lambda i:d[names[i]],range(permLength))))
-# list(map(lambda x:print('{:16}'.format(x),'{:>6}'.format(newD[x])),dict(sorted(newD.items(),key=lambda x:x[0]))))
+"""
